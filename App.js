e47c8ff6812ca03edf2c03c858c1c141f1af4e2d
Lauren <laurenb252@outlook.com>
2021-09-11 11:32:52 +0100

    Implemented frontend design
    Implemented frontend design for the sign in screen, as seen on Figma. Screen has been tested on an iPhone X, SE and iPad Air 2 to test its responsiveness. Yet to be tested on Android.

--- App.js
+++ App.js
@@ -3,18 +3,40 @@ import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import SignInScreen from './screens/sign-in.js';
+
+const getFonts = () => Font.loadAsync({
+  'Nunito-Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  'Nunito-Light': require('./assets/fonts/Nunito-Light.ttf'),
+  'Nunito-ExtraLight': require('./assets/fonts/Nunito-ExtraLight.ttf'),
+  'Nunito-SemiBold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+});
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="SignIn" component={SignInScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [fontsLoaded, setFontsLoaded] = React.useState(false);
+  if (fontsLoaded) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen options={{ headerShown: false }} name="SignIn" component={SignInScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={() => console.log('error')}
+      />
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
--- screens/sign-in.js
+++ screens/sign-in.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Touchable } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import AnimatedLottieView from 'lottie-react-native';
-import { TextInput } from 'react-native-paper';
 
 export default function SignIn() {
     return (
-        <View style={{backgroundColor: '#CDE0F1', flex: 1}}>
+        <View style={{ backgroundColor: '#CDE0F1', flex: 1 }}>
             <View style={styles.animationContainer}>
                 <AnimatedLottieView style={styles.fishAnimation} source={require('../assets/fish-tank.json')} autoPlay resizeMode='contain' />
             </View>
@@ -16,14 +15,29 @@ export default function SignIn() {
                 <Text style={styles.continue}>Sign in to continue</Text>
             </View>
             <View style={styles.inputContainer}>
-                <Text>Email address</Text>
+                <Text style={styles.inputLabels}>Email address</Text>
                 <TextInput style={styles.textInput} placeholder="johndoe@gmail.com"></TextInput>
-                <Text>Password</Text>
+                <Text style={styles.inputLabels}>Password</Text>
                 <TextInput style={styles.textInput} placeholder="************"></TextInput>
             </View>
+            <View style={styles.forgottenPTextContainer}>
+                <TouchableOpacity>
+                    <Text style={styles.forgottenPText}>Forgotten password?</Text>
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity>
+                <View style={styles.signInBtn}>
+                    <Text style={styles.signInText}>Sign in</Text>
+                </View>
+            </TouchableOpacity>
+            <View style={styles.bottomTextContainer}>
+                <Text style={styles.bottomText}>Don't have an account?</Text>
+                <TouchableOpacity>
+                    <Text style={styles.bottomText}>Create account</Text>
+                </TouchableOpacity>
 
+            </View>
             <StatusBar style="auto" />
-
         </View>
 
     );
@@ -33,22 +47,29 @@ const styles = StyleSheet.create({
     animationContainer: {
         height: hp('30%'),
         width: wp('100%'),
+        position: 'relative',
     },
     fishAnimation: {
+        width: wp('100%'),
+        height: hp('50%'),
+        marginTop: '-6%',
+        alignSelf: 'center'
     },
     textContainer: {
         height: hp('10%'),
         width: hp('100%'),
     },
     welcome: {
-        fontSize: hp('6%'),
+        fontSize: hp('5.5%'),
         position: 'relative',
         left: 20,
+        fontFamily: 'Nunito-Bold',
     },
     continue: {
         fontSize: hp('2%'),
         position: 'relative',
         left: 30,
+        fontFamily: 'Nunito-Regular',
     },
     inputContainer: {
         width: wp('80%'),
@@ -57,5 +78,48 @@ const styles = StyleSheet.create({
     },
     textInput: {
         marginBottom: '5%',
-    }
+        borderColor: '#85AFD6',
+        borderWidth: 2,
+        borderRadius: 10,
+        height: hp('6.5%'),
+        backgroundColor: '#ECECEC',
+        paddingLeft: '3%',
+    },
+    inputLabels: {
+        fontFamily: 'Nunito-Light',
+        marginBottom: '1.5%',
+    },
+    forgottenPTextContainer: {
+        width: wp('80%'),
+        alignItems: 'flex-end',
+        alignSelf: 'center',
+        marginTop: '-2.5%',
+    },
+    forgottenPText: {
+        fontFamily: 'Nunito-SemiBold',
+    },
+    signInBtn: {
+        alignSelf: 'center',
+        backgroundColor: '#85AFD6',
+        width: wp('60%'),
+        height: hp('7%'),
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '10%',
+        borderRadius: 10,
+    },
+    signInText: {
+        fontFamily: 'Nunito-SemiBold',
+        textAlign: 'center',
+        fontSize: hp('2.5%'),
+    },
+    bottomTextContainer: {
+        alignItems: 'center',
+        marginTop: hp('6%'),
+    },
+    bottomText: {
+        fontFamily: 'Nunito-SemiBold',
+        fontSize: hp('2%'),
+        marginBottom: hp('1%'),
+    },
 });
